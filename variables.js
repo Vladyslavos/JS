82c76aec5b57b109a8cc15f3537532dfeafb30e1
Vladyslav <vladyslawork@gmail.com>
2021-01-16 22:36:13 +0200

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,39 @@
+"use strict" //Strict Mode On
+
+//Variables
+
+let weather = '18';
+weather = '17'
+let wind = weather;
+alert(wind);
+
+
+let cash = 120,
+    money = 176,
+    euros = 655;
+
+
+
+let MyAge = 21;
+let MyNewAge = 17;
+MyAge = MyNewAge;
+console.log(MyAge);
+
+
+
+const COLOR_WHITE = '#fff'; //It is customary to write the names of constants with a predetermined value in uppercase.
+const FOOTER_SIZE = 25;
+
+
+
+const UserInfo = {
+	name: 'Vladyslav',
+	age: 21,
+	message: 'Hello!'
+}
+console.log(UserInfo);
+
+//Then we will change the value 
+UserInfo.age = 45;
+//In console we will see new value
+console.log(UserInfo);
