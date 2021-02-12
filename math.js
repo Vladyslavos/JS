15ba55debeb2ab1193122d4f71bc9a8cbec54de7
Vladyslav <vladyslawork@gmail.com>
2021-02-12 15:58:15 +0200

    Add files via upload

---
+++ math.js
@@ -0,0 +1,11 @@
+let a = 19;
+let b = '23';
+let c = a + b;
+console.info(c); //Result will be 1923
+
+
+console.log(Math.PI < 4);//true
+
+console.log(Math.max (2, 6, 9, 11, 1237, 0)); // 1237
+
+console.log(Math.min (90, 76, 223, 1, 0, 663));// 0
