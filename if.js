443b083cc8943741441382712b81c89848fb77a6
Vladyslav <vladyslawork@gmail.com>
2021-02-13 23:02:02 +0200

    Add files via upload

---
+++ if.js
@@ -0,0 +1,19 @@
+/*let usersage = prompt('Hello! How old are you?');
+if (usersage == ''){
+    prompt('Hello! How old are you?');
+}
+if (usersage >= 18){
+    alert('Feel free!');
+}
+else {
+    alert('Come when you turn 18!');
+}*/
+
+/*let joe = 'word';
+
+switch (joe) {
+    case "word":
+        console.log('Hello!');
+        break;
+}
+ */
