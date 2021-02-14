c1f8d7ca8abe12f9fb2e5a8d20ea9c3d24faec10
Vladyslav <vladyslawork@gmail.com>
2021-02-14 23:12:30 +0200

    Add files via upload

---
+++ array.js
@@ -0,0 +1,16 @@
+let arr = new Array(1, 4, true, 'strokas', 0, -100);
+console.log(arr);
+//
+let arr_1 = [1, 4, true, 'strokas', 0, -100];
+arr_1[2] = false;
+console.log(arr_1);
+//
+let arr_3 = [[1, 4, true, 'strokas', 0, -100], [0], [true]];
+console.log(arr_3[1]);
+//
+let arr_4 = [1, 4, 5, 66, 0, false,true, 9];
+console.log(arr_4.length);
+//
+let matrix =[[34,55,77], [909,76,55],[7,9,1]];
+matrix[1][1] = true;
+console.log(matrix);
