5c07ad497adc21680a73d98e306e03e740c300ad
Vladyslav <vladyslawork@gmail.com>
2021-02-17 23:28:38 +0200

    Update cycles.js

--- cycles.js
+++ cycles.js
@@ -1,4 +1,20 @@
+/*
 for (let i = 0; i < 10; i++){
     console.log(i);
 }
-while
+*/
+/*
+let usersName = prompt('Hello! What is your name?');
+while (usersName === '' || usersName === null){
+    prompt('Hello! What is your name?');
+}
+let usersAge = prompt('Hello' + usersName + '! How old are you?');
+while (usersAge === '' || usersAge === null){
+    prompt('Hello ' + usersName + '! How old are you?');
+}
+if (usersAge >= 18){
+    alert('Feel free!');
+} else {
+    alert('Come when you will be 18!');
+}
+*/
