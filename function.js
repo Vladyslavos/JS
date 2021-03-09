4efbccf1b72234ab9a95e0cfe66fb99eae06f975
Vladyslav <vladyslawork@gmail.com>
2021-03-09 17:56:12 +0200

    Update function.js

--- function.js
+++ function.js
@@ -1,8 +1,30 @@
-//Function 1
-function usersInfo(a, b) {
-	let res = a + b;
-	console.log(res);
 
-
+/*let usersName = prompt('Hello! What is your name?');
+if (usersName == '') {
+	('Hello! What is your name?');
 }
-usersInfo(8889);
+let usersAge = prompt(usersName + ', how old are you?');
+if (usersAge == '') {
+	prompt(usersName + ', how old are you?');
+}
+let usersWay = prompt(usersName + ', choose your way! If its Frontend, write "Front", if its backend write "Back"');
+if (usersWay == 'Front') {
+	frontend();
+}else{
+	backend();
+}
+
+
+
+//Functions
+function frontend(){
+	let usersExp = prompt(usersName + ', do you have some expereince in this field? Write "Yes" or "No"');
+	if (usersExp == "Yes") {
+		alert('Ok! But weare not sure that it will be enough! Plsease visit our site to choose your study programm!');
+	}else{
+		alert('Ok! Plsease visit our site to choose your study programm!');
+	}
+}
+function backend(){
+	let usersExp = prompt(usersName + ', do you have some expereince in this field?');
+}*/
