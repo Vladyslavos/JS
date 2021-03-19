e980043ebbf1aa30c3916a43377c16229956c865
Vladyslav <vladyslawork@gmail.com>
2021-03-19 09:25:04 -1200

    Add files via upload

---
+++ control.js
@@ -0,0 +1,8 @@
+let changes = document.getElementById("text");
+changes.style.color = "red";
+//
+document.getElementById("link").style.backgroundColor = "green";
+//
+let list = document.getElementById("column");
+list.style.marginTop = "440px";
+//
