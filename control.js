e4dba45fc03eae3cda05f8f5cee2bc247fbdf826
Vladyslav <vladyslawork@gmail.com>
2021-03-20 21:34:59 +0200

    Update control.js

--- control.js
+++ control.js
@@ -6,3 +6,5 @@ document.getElementById("link").style.backgroundColor = "green";
 let list = document.getElementById("column");
 list.style.marginTop = "440px";
 //
+let footerText = document.getElementById("foot");
+footerText.style.fontSize = "400px";
