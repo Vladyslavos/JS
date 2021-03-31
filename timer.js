ef072d311b997509e7fa2a849817e669c6091062
Vladyslav <vladyslawork@gmail.com>
2021-03-31 18:41:45 +0300

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,9 +1,16 @@
-setInterval(my_func, 7000);
-
-function my_func(){
-	alert('Hello!')
-}
-
 let counter = 0;
-let id = setInterval()
+function count(){
+	counter++;
+	console.log("Counter:" + counter);
+}
+setInterval(count, 1000);
+//We can create function inside setInterval();
+let counter_1 = 0;
+setInterval(function count_2(){
+    counter_1++;
+	console.log("Counter:" + counter_1);
+}, 2000);
 //
+setTimeout(function greetings(){
+	alert('Hello world!');
+}, 3000);
