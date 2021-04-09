667fc20796573f9bd0d5f1c36509e5d7df720470
Vladyslav <vladyslawork@gmail.com>
2021-04-09 23:51:30 +0300

    Update objects.js

--- objects.js
+++ objects.js
@@ -1,10 +1,13 @@
 //Time
 let date = new Date();
-
 console.log(date.getFullYear());
 console.log(date.getMonth());
+console.log(date.getHours());
+console.log(date.getSeconds());
 console.log(date.getMinutes());
 console.log(date.getSeconds());
+
+console.log('Time: ' + date.getHours() + ':' + date.getMinutes() )
 
 
 
