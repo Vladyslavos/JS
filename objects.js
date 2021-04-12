f758cdb03c7d5a20b23e9c8436486084c9d15d8c
Vladyslav <vladyslawork@gmail.com>
2021-04-12 20:30:42 +0300

    Update objects.js

--- objects.js
+++ objects.js
@@ -33,10 +33,10 @@ class Phone{
 	}
 }
 
+let iphone = new Phone("Iphone", "0,129 kg", "34mpx");
+console.log(iphone.camera);
+
 class Cars{
 	constructor(price, year, color)
 	
 }
-
-let iphone = new Phone("Iphone", "0,129 kg", "34mpx");
-console.log(iphone.camera);
